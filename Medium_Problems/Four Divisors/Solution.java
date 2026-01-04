f8878c1b95365850cb376a63cdfdb428fe1402db
Vasandhan P K G <vasandhanpkg@gmail.com>
2026-01-04 22:22:06 +0530

    Implement method to sum four divisors of numbers

---
+++ Medium_Problems/Four Divisors/Solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int sumFourDivisors(int[] nums) {
+        int tot_sum=0;
+        for(int n:nums)
+        {
+            int count=0;
+            int sum=0;
+            for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                if (i == n / i) {
+                count++;
+                sum += i;
+        }    else {
+            count += 2;
+            sum += i + n / i;
+        }
+    }
+}
+            if(count==4)
+            {
+            tot_sum+=sum;
+            }
+
+        }
+        return tot_sum;
+    }
+   
+        
+       
+    
+}
