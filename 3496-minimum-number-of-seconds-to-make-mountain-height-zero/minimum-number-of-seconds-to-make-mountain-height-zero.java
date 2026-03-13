282d4454abd2275f3a79225fec6e23c189c73060
Vasandhan P K G <vasandhanpkg@gmail.com>
2026-03-13 23:22:44 +0530

    Time: 6 ms (100.00%) | Memory: 47.8 MB (46.46%) - LeetSync

--- 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.java
+++ 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.java
@@ -1,41 +1,26 @@
 class Solution {
     public long minNumberOfSeconds(int mountainHeight, int[] workerTimes) {
-        long left = 1;
-        
-        int min_time = Integer.MAX_VALUE;
-        for (int wt : workerTimes) {
-        	min_time = Math.min(min_time, wt);
-        }
-        
-        long right = (long)min_time * (long)mountainHeight * (mountainHeight + 1) / 2;
-        long answer = right;
+
+        int max = 0;
+        for (int x : workerTimes) 
+            max = Math.max(max, x);
+
+        int h = (mountainHeight-1) / workerTimes.length + 1;
+        long left = 1, right = (long) max * h * (h + 1) / 2;
         
         while (left <= right) {
-        	long mid = left + (right - left) / 2;
-        	
-        	if (can_achieve(mid, mountainHeight, workerTimes)) {
-        		answer = mid;
-        		right = mid - 1;
-        	} else {
-        		left = mid + 1;
-        	}
+            long mid = left + (right - left) / 2;
+            if (check(mountainHeight, workerTimes, mid)) right = mid - 1;
+            else left = mid + 1;
         }
-        
-        return answer;
+        return left;
     }
-    
-    private boolean can_achieve(long time, int mountainHeight, int[] workerTimes) {
-    	long total_height = 0;
-    	
-    	for (int wt : workerTimes) {
-    		double D = 1 + 8.0 * time / wt;
-    		long x = (long)((-1 + Math.sqrt(D)) / 2);
-    		
-    		total_height += x;
-    		
-    		if (total_height >= mountainHeight) return true;
-    	}
-    	
-    	return total_height >= mountainHeight;
+
+    boolean check(int mountainHeight, int[] workerTimes, long time) {
+        for (int x : workerTimes) {
+            mountainHeight -= (int)(-1 + Math.sqrt(1 + 8 * time / x)) / 2;
+            if (mountainHeight <= 0) return true;
+        }
+        return false;
     }
 }
