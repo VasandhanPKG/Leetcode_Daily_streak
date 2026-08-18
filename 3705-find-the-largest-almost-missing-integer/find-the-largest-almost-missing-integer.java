6b77692addae8f9838dc9f3cd74c46ebbcef8e7d
Vasandhan P K G <vasandhanpkg@gmail.com>
2026-08-18 09:14:50 +0530

    Time: 1 ms (99.30%) | Memory: 44.8 MB (71.93%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
@@ -1,31 +1,29 @@
 class Solution {
     public int largestInteger(int[] nums, int k) {
-        int n=nums.length;
-        int freq[]=new int[51];
-        int max=0;
-        for(int num:nums)
-        {
+        int n = nums.length;
+        int freq[] = new int[51];
+        int max = 0;
+        for (int num : nums) {
             freq[num]++;
-            max=Math.max(max,num);
+            max = Math.max(max, num);
         }
-        int ans=0;
-        if(k==n) return max; 
-        if(k==1) 
-        {
-            for(int i=50;i>=0;i--)
-            {
-                if(freq[i]==1) return i;
+        int ans = 0;
+        if (k == n)
+            return max;
+        if (k == 1) {
+            for (int i = 50; i >= 0; i--) {
+                if (freq[i] == 1)
+                    return i;
             }
         }
-        if(freq[nums[n-1]]==1 && freq[nums[0]]==1) 
-        {
-            return (nums[0]>nums[n-1])? nums[0]:nums[n-1];
+        if (freq[nums[n - 1]] == 1 && freq[nums[0]] == 1) {
+            return (nums[0] > nums[n - 1]) ? nums[0] : nums[n - 1];
         }
-        if(freq[nums[n-1]]==1 ) return nums[n-1];
-        if(freq[nums[0]]==1) return nums[0];
+        if (freq[nums[n - 1]] == 1)
+            return nums[n - 1];
+        if (freq[nums[0]] == 1)
+            return nums[0];
         return -1;
-        
-            
-      
+
     }
 }
